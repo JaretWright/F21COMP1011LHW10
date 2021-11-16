9aab8b2dc06256e11760e02f80f79208edd112c9
Jaret Wright <Jaret.Wright@GeorgianCollege.ca>
2021-11-16 13:03:14 -0500

    Added model, view, controller and API utilities

---
+++ src/main/java/com/example/f21comp1011lhw10/SearchViewController.java
@@ -0,0 +1,4 @@
+package com.example.f21comp1011lhw10;
+
+public class SearchViewController {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,9 @@ module com.example.f21comp1011lhw10 {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires com.google.gson;
 
 
-    opens com.example.f21comp1011lhw10 to javafx.fxml;
+    opens com.example.f21comp1011lhw10 to javafx.fxml, com.google.gson;
     exports com.example.f21comp1011lhw10;
 }
